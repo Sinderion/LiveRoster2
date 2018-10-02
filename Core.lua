2c8a57285e92a9936e375e5b95aed41f9dd9e849
Sinderion <jwinsatt@yahoo.com>
2018-10-02 06:41:29 -0400

    Basic aceDB added.

--- Core.lua
+++ Core.lua
@@ -1,14 +1,26 @@
 
-LiveRoster = LibStub("AceAddon-3.0"):NewAddon("LiveRoster");
-LiveRoster.Guild = LR_LiveRoster_Class:New();
-LiveRoster.Guild.Roster:UpdateRoster();
-local TestToon = "Sinderion-ShadowCouncil";
-local Character = LiveRoster.Guild.Roster:GetCharacterByName(TestToon);
-print(Character:GetRank());
+LiveRoster = LibStub("AceAddon-3.0"):NewAddon("LiveRoster", "AceConsole-3.0");
+
 
 function LiveRoster:OnInitialize()
-	print("LiveRoster Initialized.")
+	print("LiveRoster Initialized.");
+	LiveRoster.Guild = LR_LiveRoster_Class:New();
+	LiveRoster.Guild.Roster:UpdateRoster();
+	self.db = LibStub("AceDB-3.0"):New("LiveRoster2DB");
+	if self.db.global.Testme == 6 then
+		LiveRoster:Print("Found test!");
+	else
+		LiveRoster:Print("No test! "..tostring(LiveRoster2DB.global.Testme));
+		self.db.global.Testme = 6;
+	end
+	
+	local TestToon = "Sinderion-ShadowCouncil";
+	local Character = LiveRoster.Guild.Roster:GetCharacterByName(TestToon);
+	print(Character:GetRank());
+	
+	
   -- Code that you want to run when the addon is first loaded goes here.
+	-- eventually syncronize alt guild info, etc.
 end
 
 function LiveRoster:OnEnable()
@@ -19,3 +31,9 @@ function LiveRoster:OnDisable()
     -- Called when the addon is disabled
 end
 
+
+--[[ Ideas
+
+- Logging of changes.
+
+]]--
